501c51f9766f23100230e921b0c7e36231d0185c
Nader Zare <nader.zare88@gmail.com>
2017-10-27 01:20:33 +0330

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+echo "RCSS Server for Student League"
+echo "Technical Committee, Soccer 2D Simulation, IranOpen"
+echo "Nader Zare, nader.zare88@gmail.com"
+
 sudo apt-get install g++ build-essential libboost-all-dev qt4-dev-tools libaudio-dev libgtk-3-dev libxt-dev
 
 sudo apt-add-repository ppa:gnurubuntu/rubuntu
@@ -8,11 +12,21 @@ sudo apt-get update
 
 sudo apt-get install rcssserver rcssmonitor rcsslogplayer
 
-mkdir ~/.rcssserver | a
-echo "make .rcssserver"
-mkdir ~/Documents/log | a
-echo "make log"
+file="~/.rcssserver"
+if [ -e "$file" ]; then
+	
+else
+	mkdir ~/.rcssserver	
+	echo "make .rcssserver"
+fi
 
+file="~/Documents/log"
+if [ -e "$file" ]; then
+        
+else
+        mkdir ~/Documents/log
+        echo "make log dir"
+fi
 
 sudo cp server.conf ~/.rcssserver/
 
